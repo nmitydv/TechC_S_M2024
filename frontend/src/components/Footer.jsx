022af17117fbf98e426f2663762018646493168f
priyankagour04 <priyankag.bba2022@ssism.org>
2024-04-05 13:24:31 +0530

    Footer has been created

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,88 @@
+import React from "react";
+import logo from "../assets/logo.svg";
+import { Link } from "react-router-dom";
+import { routesConstant } from "../routes/AppRoutesConstants";
+import "../components/Footer.css";
+import instagram from "../assets/instagram 1.svg";
+import facebook from "../assets/facebook 1.svg";
+import twitter from "../assets/twitter 1.svg";
+import linkedin from "../assets/linkedin 1.svg";
+import whatsapp from "../assets/whatsapp 1.svg";
+import { LiaCopyright } from "react-icons/lia";
+const Footer = () => {
+  return (
+    <>
+      <div className="footer_ctn">
+        <div className="container">
+          <div className="row">
+            <div className="col-9 d-flex">
+              <div className="col-5">
+                <img className="" src={logo}></img>
+              </div>
+              <div className="col-6 d-flex p-4 text-dark footer_tags">
+                <div className="col">
+                  {" "}
+                  <Link
+                    to={routesConstant.Home.path}
+                    className="nav-link"
+                  >
+                    Home{" "}
+                  </Link>
+                </div>
+                <div className="col">
+                  {" "}
+                  <Link to={routesConstant.About.path} className="nav-link">
+                    About
+                  </Link>
+                </div>
+                <div className="col me-2">
+                  {" "}
+                  <Link to={routesConstant.Portfolio.path} className="nav-link">
+                    Portfolio
+                  </Link>
+                </div>
+                <div className="col mx-2">
+                  {" "}
+                  <Link to={routesConstant.Portfolio.path} className="nav-link">
+                    Services
+                  </Link>
+                </div>
+                <div className="col">
+                  {" "}
+                  <Link to={routesConstant.Portfolio.path} className="nav-link">
+                    Product
+                  </Link>
+                </div>
+              </div>
+            </div>
+            <div className="col-3 social_logo  d-flex mt-2 justify-content-between">
+              <div className="mx-2 mt-1">
+                <img src={instagram} />
+              </div>
+              <div className="mx-2 mt-1">
+                <img src={facebook} />
+              </div>
+              <div className="mx-2 mt-1">
+                <img src={twitter} />
+              </div>
+              <div className="mx-2 mt-1">
+                <img src={linkedin} />
+              </div>
+              <div className="">
+                <img src={whatsapp} />
+              </div>
+            </div>
+          </div>
+          <hr className="horizantal_line my-5 "></hr>
+          <div className="text-center text-secondary">
+            {" "}
+            Copyright <LiaCopyright />
+            2024 Amourtechcreations || All Right Reserved
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,44 +1,74 @@
 // all routing parts are here
 
 // eslint-disable-next-line no-unused-vars
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { routesConstant } from './AppRoutesConstants'
-import HomePage from '../pages/HomePage'
-import AboutPage from '../pages/AboutPage'
-import PortfolioPage from '../pages/PortfolioPage'
-import ContactPage from '../pages/ContactPage'
-import WebDesignPage from '../pages/WebDesignPage'
-import WebDevelopmentPage from '../pages/WebDevelopmentPage'
-import AppDesignPage from '../pages/AppDesignPage'
-import AppDevelopmentPage from '../pages/AppDevelopmentPage'
-import UiUxPage from '../pages/UiUxPage'
-import GraphicsDesignPage from '../pages/GraphicsDesignPage'
-import ContentCreationPage from '../pages/ContentCreationPage'
-import NavbarComponent from '../components/navbar/NavbarComponent'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { routesConstant } from "./AppRoutesConstants";
+import HomePage from "../pages/HomePage";
+import AboutPage from "../pages/AboutPage";
+import PortfolioPage from "../pages/PortfolioPage";
+import ContactPage from "../pages/ContactPage";
+import WebDesignPage from "../pages/WebDesignPage";
+import WebDevelopmentPage from "../pages/WebDevelopmentPage";
+import AppDesignPage from "../pages/AppDesignPage";
+import AppDevelopmentPage from "../pages/AppDevelopmentPage";
+import UiUxPage from "../pages/UiUxPage";
+import GraphicsDesignPage from "../pages/GraphicsDesignPage";
+import ContentCreationPage from "../pages/ContentCreationPage";
+import NavbarComponent from "../components/navbar/NavbarComponent";
+import Footer from "../components/Footer";
 
 export const AppRoutes = () => {
-    return (
-        <>
-           <BrowserRouter>
-    <Routes>
-        {/* Public Routing */}
-        <Route path={routesConstant.Home.path} element={<NavbarComponent><HomePage /></NavbarComponent>} />
-        <Route path={routesConstant.About.path} element={<AboutPage />} />
-        <Route path={routesConstant.Portfolio.path} element={<PortfolioPage />} />
-        <Route path={routesConstant.Contact.path} element={<ContactPage />} />
-        <Route path={routesConstant.WebDesign.path} element={<WebDesignPage />} />
-        <Route path={routesConstant.WebDevelopment.path} element={<WebDevelopmentPage />} />
-        <Route path={routesConstant.AppDesign.path} element={<AppDesignPage />} />
-        <Route path={routesConstant.AppDevelopment.path} element={<AppDevelopmentPage />} />
-        <Route path={routesConstant.Ui_Ux_Design.path} element={<UiUxPage />} />
-        <Route path={routesConstant.GraphicDesign.path} element={<GraphicsDesignPage />} />
-        <Route path={routesConstant.ContentCreation.path} element={<ContentCreationPage />} />
-
-
-    </Routes>
-</BrowserRouter>
-
-        </>
-    )
-}
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          {/* Public Routing */}
+          {/* <Route path={routesConstant.Home.path} element={<NavbarComponent><HomePage /></NavbarComponent>} /> */}
+          <Route path={routesConstant.About.path} element={<AboutPage />} />
+          <Route
+            path={routesConstant.Portfolio.path}
+            element={<PortfolioPage />}
+          />
+          <Route path={routesConstant.Contact.path} element={<ContactPage />} />
+          <Route
+            path={routesConstant.WebDesign.path}
+            element={<WebDesignPage />}
+          />
+          <Route
+            path={routesConstant.WebDevelopment.path}
+            element={<WebDevelopmentPage />}
+          />
+          <Route
+            path={routesConstant.AppDesign.path}
+            element={<AppDesignPage />}
+          />
+          <Route
+            path={routesConstant.AppDevelopment.path}
+            element={<AppDevelopmentPage />}
+          />
+          <Route
+            path={routesConstant.Ui_Ux_Design.path}
+            element={<UiUxPage />}
+          />
+          <Route
+            path={routesConstant.GraphicDesign.path}
+            element={<GraphicsDesignPage />}
+          />
+          <Route
+            path={routesConstant.ContentCreation.path}
+            element={<ContentCreationPage />}
+          />
+          <Route
+            path={routesConstant.Home.path}
+            element={
+              <Footer>
+                <HomePage />
+              </Footer>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
